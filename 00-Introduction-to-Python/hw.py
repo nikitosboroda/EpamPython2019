2c540659e2d0bb0bc170c598b50f206712fc2dff
Nikita <39776787+nikitosboroda@users.noreply.github.com>
2019-05-19 22:57:56 +0300

    Update hw.py

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -9,6 +9,9 @@
 
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+	new_a = list(a)
+	new_b = list(b)
+	return sorted(new_a) == sorted(new_b)
 
 
 assert is_permutation('baba', 'abab')
