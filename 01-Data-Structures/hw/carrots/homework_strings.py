8b3be6c25624d4e45f62dade32983be4d0f2bbdd
Nikita <39776787+nikitosboroda@users.noreply.github.com>
2019-06-05 18:53:10 +0300

    !

--- 01-Data-Structures/hw/carrots/homework_strings.py
+++ 01-Data-Structures/hw/carrots/homework_strings.py
@@ -32,7 +32,8 @@
 P.S. За незакрытый файловый дескриптор - караем штрафным дезе.
 
 """
-
+import matplotlib.pyplot as plt
+import time
 
 def translate_from_dna_to_rna(dna):
     with open("./out/dna_to_rna.txt", "w") as dna_to_rna:
@@ -54,9 +55,11 @@ def translate_from_dna_to_rna(dna):
 def count_nucleotides(dna):
     count_nucl = open('./out/count_nucl1.txt', 'w')
     num_of_nucleotides = {i: 0 for i in 'ACGT'}
+    list_nucls = []
     for line in dna:
         if line.startswith('>'):
             if num_of_nucleotides['A'] != 0:
+                list_nucls += [num_of_nucleotides]
                 count_nucl.write(str(num_of_nucleotides)+'\n')
             num_of_nucleotides = {i: 0 for i in 'ACGT'}
             count_nucl.write(line)
@@ -65,6 +68,8 @@ def count_nucleotides(dna):
             num_of_nucleotides[sym] += line.count(sym)
     count_nucl.write(str(num_of_nucleotides)+'\n')
     count_nucl.close()
+    list_nucls += [num_of_nucleotides]
+    return list_nucls
 
 
 def translate_rna_to_protein(rna):
@@ -78,20 +83,39 @@ def translate_rna_to_protein(rna):
     protein = open('prottttt.txt', 'w')
     for i in range(len(rna)):
         # print('IIIII',i)
+        protein.write(f'Gen # {i} \n')
         for j in range(0, len(rna[i]), 3):
             # print('JJJJJ',j)
             if rna[i][j:j+3] not in codon:
                 continue
             protein.write(codon[rna[i][j:j+3]])
+        protein.write('\n')
     protein.close()
 
 
 with open('./files/dna.fasta') as dna:
-    count_nucleotides(dna)
+    nucleotides = count_nucleotides(dna)
+    print(nucleotides)
+    list_of_counted = [[i[j] for j in i] for i in nucleotides]
+    print(list_of_counted)
+    plt.style.use('ggplot')
+    x = [i for i in 'ACGT']
+    x_pos = [i for i, _ in enumerate(x)]
+
+    for ind, key in enumerate(list_of_counted):
+        plt.bar(x_pos, key, color='red')
+        plt.xlabel("Nucleotides")
+        plt.ylabel("Count")
+        plt.title(f"Counted nucls in Gen {ind}")
+
+        plt.xticks(x_pos, x)
+        plt.show()
+        # time.sleep(10)
+
 
 
 with open('./files/dna.fasta') as dna:
     rna = translate_from_dna_to_rna(dna)
-    # print(rna)
-    prot = translate_rna_to_protein(rna)
+    #print(rna)
+    translate_rna_to_protein(rna)
     #print('PROTEIN', prot)
