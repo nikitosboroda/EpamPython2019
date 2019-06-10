fd3c114118e4661c2ebe57c6fd4856e33e307add
Nikita <39776787+nikitosboroda@users.noreply.github.com>
2019-06-10 21:44:54 +0300

    Update hw4_1.py

--- 03-fp-decorator/hw/hw4_1.py
+++ 03-fp-decorator/hw/hw4_1.py
@@ -10,9 +10,8 @@ def problem9():
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product: abc.
     """
-    return [(a, b, (1000 - a - b)) for a in range(1, 1001) for b in range(a, 1001)
-           if a ** 2 + b ** 2 == (1000 - a - b) ** 2]
-
+    return reduce(lambda x, y: x*y, [[a, b, 1000 - a - b] for a in range(1, 1001) for b in range(a, 1001)
+           if a ** 2 + b ** 2 == (1000 - a - b) ** 2][0])
 
 def problem6():
     """
